27efcb1b6fa4c8422a9caf9ed5943e54482b5ace
Majdi Saibi <saibimajdi@outlook.com>
2016-10-07 17:54:02 +0100

    Update server.js
    fixing bug

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+server.post('/api/messages/', connector.listen());
 
 //=========================================================
 // Bots Dialogs
