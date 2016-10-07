354582b1a8c43005ff5fc401182c8cec74dcb1bf
Majdi Saibi <saibimajdi@outlook.com>
2016-10-07 22:29:31 +0100

    default

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 var restify = require('restify');
-var builder = require('botbuilder');
-
-//=========================================================
-// Bot Setup
-//=========================================================
-
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
+ var builder = require('botbuilder');
+ 
+ //=========================================================
+ // Bot Setup
+ //=========================================================
+ 
+ // Setup Restify Server
+ var server = restify.createServer();
+ server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+ });
+   
+ // Create chat bot
+  var connector = new builder.ChatConnector({
+      appId: process.env.MICROSOFT_APP_ID,
+      appPassword: process.env.MICROSOFT_APP_PASSWORD
+  });
+  //var connector = new builder.ConsoleConnector().listen();
+  var bot = new builder.UniversalBot(connector);
+  server.post('/api/messages/', connector.listen());
   
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages/', connector.listen());
-
 //=========================================================
 // Bots functions
 //=========================================================
-
+/*
 function execute(query){
     if(!query)
         return null;
@@ -47,35 +47,49 @@ function execute(query){
     if(Number.isInteger(num1) && Number.isInteger(num2)){
         return num1 + num2;
     }
-    
+
     return null;
 }
-
+*/
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
 bot.dialog('/', [
-    function (session) {
-        if(!session.userData.name){
-            builder.Prompts.text('Hi there, what\'s your name ?');
-        }
-        else{
-            next({response : session.userData.name});
+     function(session){
+         session.beginDialog('/ensureProfile',session.userData.profile);
+     },
+     function(session, result){
+         session.userData.profile = result.response;
+         session.send('Hello %(name)s',session.userData.profile);
+     }
+ ]);
+
+
+bot.dialog('/ensureProfile',[
+    function(session, args, next){
+        session.dialogData.profile = args || {};
+        if(!session.dialogData.profile.name){
+            builder.Prompts.text(session,"What's your name ?");
+        }else{
+            next();
         }
     },
-    function(session,result){
-        var name = result.response;
-        session.send('nice to meet you %s',name);
-        var introMessage = 'I\'m a bot designed & develped by Majdi!';
-        introMessage += '\nI still learning a lot from Majdi, but now I can do those actions only : ';
-        introMessage += '\n * calculate NUMBER + NUMBER (ex: calculate 1 + 5)';
-    }
-    ,function(session,result){
+    function(session, result, next){
         if(result.response){
-            var query = result.response;
-            var botResponse = execute(query);
+            session.dialogData.profile.name = result.response;
         }
+        if(!session.dialogData.profile.company){
+            builder.Prompts.text(session, "What company do you work for?");
+        }else{
+            next();
+        }
+    },
+    function(session, result){
+        if(result.response){
+            session.dialogData.profile.company = result.response;
+        }
+        session.endDialogWithResult({ response : session.dialogData.profile});
     }
 ]);
